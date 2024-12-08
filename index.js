186f1c38dd732f3bd510834f9eee7586fa6d6673
Smit <125031715+smit763@users.noreply.github.com>
2024-12-08 13:13:30 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors")
 const connectDB = require('./utils/db');
 const bookRoutes = require('./routes/bookRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// Middleware
+app.use(cors())
 app.use(express.json());
 app.use(express.json());
 
-// Routes
+
 app.use('/api/v1', bookRoutes);
 app.use('/api/v1', userRoutes);
 
